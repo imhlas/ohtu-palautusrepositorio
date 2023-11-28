c4a61d7405cd2074d1542e4a936f5b4aa7c873d0
Iida Lassila <iida.m.lassila@helsinki.fi>
2023-11-28 16:36:51 +0200

    Add variable SCORE_NAMES and modify printing in get_score

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,4 +1,5 @@
 class TennisGame:
+    SCORE_NAMES = ["Love", "Fifteen", "Thirty", "Forty"]
     ONE_POINT = 1
 
     def __init__(self, player1_name, player2_name):
@@ -14,33 +15,14 @@ class TennisGame:
             self.player2_score = self.player2_score + 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
-
         if self.player1_score == self.player2_score:
-            score = self.score_tie()
+            return self.score_tie()
 
         elif self.player1_score >= 4 or self.player2_score >= 4:
-            score = self.score_advantage_or_win()
+            return self.score_advantage_or_win()
 
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.player1_score
-                else:
-                    score = score + "-"
-                    temp_score = self.player2_score
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
-        return score
+            return f"{self.SCORE_NAMES[self.player1_score]}-{self.SCORE_NAMES[self.player2_score]}"
 
     def score_tie(self):
         scores = ["Love-All", "Fifteen-All", "Thirty-All"]
@@ -57,3 +39,4 @@ class TennisGame:
             return "Win for player1"
         else:
             return "Win for player2"
+
